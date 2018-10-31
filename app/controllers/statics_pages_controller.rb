510cf445bf985939c780377a0e1f423199aed0be
Kalisp20 <hanane.duissi@yahoo.com>
2018-10-31 17:29:56 +0100

    création page gossip/new et post gossip/new

--- app/controllers/statics_pages_controller.rb
+++ app/controllers/statics_pages_controller.rb
@@ -2,4 +2,8 @@ class StaticsPagesController < ApplicationController
     def home
         
     end
+    def new
+    end
+    def create
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
-  get '/home', to: 'statics_pages#home'
+  get '/gossips', to: 'statics_pages#home'
+  get '/gossips/new', to: 'statics_pages#new'
+  post '/gossips/new', to: 'statics_pages#create'
 
 end
